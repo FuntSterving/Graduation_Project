b0e20c911b21ac79e4ef825f3947ac0800b9ab79
FuntSterving <wesbnk@gmail.com>
2023-10-16 19:27:58 +0600

    user upd

---
+++ src/components/layouts/NavbarComponents.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="side">
+        <p-button v-if="!user" @click="googleRegister" label="Войти" icon="pi pi-sign-in" />
+
+        <p-button v-else type="button" @click="googleLogout" label="Выйти" icon=" pi pi-sign-out" />
+
+    </div>
+</template>
+
+<script setup>
+import PButton from 'primevue/button';
+import { useUser } from '@/composable/useUser'
+const { user, googleRegister, googleLogout } = useUser()
+</script>
+
+<style  scoped>
+.p-button{
+    padding: 10px;
+    margin: 10px;
+}
+</style>
